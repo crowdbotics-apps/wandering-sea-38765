e822c9bd7fb9b45bb99f8faae5b37fead78b1762
Crowdbotics <team@crowdbotics.com>
2023-02-07 04:41:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-sea-38765",
+  url: "https://wandering-sea-38765.botics.co",
+  api: "https://wandering-sea-38765.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
